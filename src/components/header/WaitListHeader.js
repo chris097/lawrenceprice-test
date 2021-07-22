cdb78addf72492bf8e04d47339c429febb11c30d
chris097 <chrisfidel.international@gmail.com>
2021-07-22 17:09:16 +0100

    fix: waitlist media query

--- src/components/header/WaitListHeader.js
+++ src/components/header/WaitListHeader.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import {HiMenu} from 'react-icons/hi';
+import {GrClose} from 'react-icons/gr';
 import { ReactComponent as LogoIcon } from '../../icons/logo-svg.svg';
 import { ReactComponent as LogInIcon } from '../../icons/login-svg.svg';
+import { useState } from 'react';
 
 export const Header = ({onClickHandler}) => {
+
+  const [ menu, setMeun ] = useState('menu');
+  const [ menuClose, setMeunClose ] = useState('');
+  const [drop, setDrop ] = useState('')
+
+  const toggleMenu = () => {
+    setMeun('')
+    setDrop('drop')
+    setMeunClose('close')
+  }
+
+  const toggleClose = () => {
+    setMeun('menu')
+    setDrop('')
+    setMeunClose('')
+  }
+
   return (
     <div className="bg-white w-full h-114 shadow-primaryShadow flex justify-between lg:px-10 px-4 items-center cursor-pointer fixed z-40">
       <div><LogoIcon /></div>
-      <div className="text-14 font-RatDisplay font-500">
-        <span className="text-secondaryColor lg:mr-10 mr-3">BBe</span>
+      <div className="text-14 font-RatDisplay whitespace-nowrap font-500">
+        <span className="text-secondaryColor lg:mr-10 mr-3 ml-5 lg:ml-0">BBe</span>
         <span className="text-primaryGray">Join Waitlist</span>
       </div>
       <div className="flex items-center">
-        <span className="text-primaryColor text-13 font-SourceCode">Verified Access</span>
-        <button onClick={onClickHandler} type="button"><LogInIcon /></button>
+        <span className="text-primaryColor text-13 font-SourceCode lg:visible invisible">Verified Access</span>
+        <button className="invisible lg:visible" onClick={onClickHandler} type="button"><LogInIcon /></button>
       </div>
+      <div className="flex items-center">
+        {menu === 'menu' && <button onClick={toggleMenu} className=" text-36 lg:invisible visible absolute right-6"><HiMenu /></button>}
+       { menuClose === 'close' && <button onClick={toggleClose} className="text-24 text-primaryGray lg:invisible visible absolute right-6"><GrClose /></button>}
+      </div>
+      { drop === 'drop' && <div className="fixed right-0 top-32 -mt-4 shadow-sm visible lg:invisible bg-white w-48 h-14">
+      <div className="flex justify-center items-center">
+      <span className="text-primaryColor text-13 font-SourceCode">Verified Access</span>
+      <button onClick={onClickHandler} type="button"><LogInIcon /></button>
+    </div>
+      </div>}
     </div>
   )
 }
--- src/components/waitList/WaitList.js
+++ src/components/waitList/WaitList.js
@@ -1,13 +1,44 @@
+import { useState } from "react";
+
 const WaitList = () => {
+  const [ mail, setMail ] = useState('mail');
+  const [phone, setPhone ] = useState('');
+
+  const toggleMail = () => {
+    setPhone('phone')
+    setMail('')
+  }
+  const togglePhone = () => {
+    setMail('mail')
+    setPhone('')
+  }
+  
   return(
     <>
-      <form className="flex justify-center mt-10 whitespace-nowrap">
-      <div className="">
-        <input className="lg:w-553 w-200 lg:h-88 h-60 border border-primaryGray rounded-full outline-none pl-6 placeholder-primaryGray text-24 font-400 font-RatDisplay" type="email" placeholder="Your email Address" />
-        <button className="lg:h-72 h-50 lg:w-172 w-91 bg-primaryBlue text-white rounded-full ml-5" type="submit">Join Waitlist</button>
-      </div>
-      </form>
-      <button className="whitespace-nowrap flex justify-center mx-auto mt-5 text-24 font-RatDisplay font-500 text-secondaryGray" type="button">I would prefer an SMS instead. </button>
+      {
+        mail === 'mail' && <div><form className="flex justify-center mt-10 whitespace-nowrap">
+        <div className="">
+          <input className="lg:w-553 w-240 lg:h-88 h-60 border border-primaryGray rounded-full outline-none pl-6 placeholder-primaryGray text-24 font-400 font-RatDisplay" type="email" placeholder="email Address" />
+          <button className="lg:h-72 h-50 lg:w-172 w-110 bg-primaryBlue text-white rounded-full ml-2" type="submit">Join Waitlist</button>
+        </div>
+        </form>
+        <button onClick={toggleMail} className="whitespace-nowrap flex justify-center mx-auto mt-5 text-24 font-RatDisplay font-500 text-secondaryGray" type="button">I would prefer an SMS instead. </button>
+        </div>
+      }
+      {
+        phone === 'phone' && <div>
+        <button onClick={togglePhone} className="whitespace-nowrap flex justify-center mx-auto mt-5 text-24 font-RatDisplay font-500 text-secondaryGray" type="button">I think an e-mail will do.</button>
+        <div>
+        <form className="flex justify-center mt-10 whitespace-nowrap">
+        <div className="">
+          <span className="text-18 font-RatDisplay lg:mr-6">Phone Number</span>
+          <input className="lg:w-553 w-167 lg:h-88 h-60 border border-primaryGray rounded-full outline-none pl-6 placeholder-primaryGray text-24 font-400 font-RatDisplay" type="email" placeholder="i.e  +234 0800 000 0000" />
+          <button className="lg:h-72 h-40 lg:w-172 w-50 bg-primaryBlue text-white lg:rounded-full rounded-2xl ml-2" type="submit">Join Waitlist</button>
+        </div>
+        </form>
+        </div>
+        </div>
+      }
     </>  
   )
 };
