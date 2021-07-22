de045e53cdf6e0e4a862c8fe4274b02ca5672a71
chris097 <chrisfidel.international@gmail.com>
2021-07-22 17:29:30 +0100

    fix: waitlist header

--- src/components/header/WaitListHeader.js
+++ src/components/header/WaitListHeader.js
@@ -24,21 +24,21 @@ export const Header = ({onClickHandler}) => {
   }
 
   return (
-    <div className="bg-white w-full h-114 shadow-primaryShadow flex justify-between lg:px-10 px-4 items-center cursor-pointer fixed z-40">
+    <div className="bg-white w-full h-114 shadow-primaryShadow flex justify-between border-2 lg:px-10 px-4 items-center cursor-pointer fixed z-40">
       <div><LogoIcon /></div>
-      <div className="text-14 font-RatDisplay whitespace-nowrap font-500">
+      <div className="text-14 font-RatDisplay whitespace-nowrap font-500 lg:-ml-32 -ml-20">
         <span className="text-secondaryColor lg:mr-10 mr-3 ml-5 lg:ml-0">BBe</span>
         <span className="text-primaryGray">Join Waitlist</span>
       </div>
-      <div className="flex items-center">
-        <span className="text-primaryColor text-13 font-SourceCode lg:visible invisible">Verified Access</span>
-        <button className="invisible lg:visible" onClick={onClickHandler} type="button"><LogInIcon /></button>
+      <div className="flex items-center invisible lg:visible absolute right-10">
+        <span className="text-primaryColor text-13 font-SourceCode">Verified Access</span>
+        <button className="" onClick={onClickHandler} type="button"><LogInIcon /></button>
       </div>
       <div className="flex items-center">
         {menu === 'menu' && <button onClick={toggleMenu} className=" text-36 lg:invisible visible absolute right-6"><HiMenu /></button>}
-       { menuClose === 'close' && <button onClick={toggleClose} className="text-24 text-primaryGray lg:invisible visible absolute right-6"><GrClose /></button>}
+       { menuClose === 'close' && <button onClick={toggleClose} className="text-24 text-primaryGray lg:hidden inline-block absolute right-6"><GrClose /></button>}
       </div>
-      { drop === 'drop' && <div className="fixed right-0 top-32 -mt-4 shadow-sm visible lg:invisible bg-white w-48 h-14">
+      { drop === 'drop' && <div className="fixed right-0 top-32 -mt-4 shadow-sm inline-block lg:hidden bg-white w-48 h-14">
       <div className="flex justify-center items-center">
       <span className="text-primaryColor text-13 font-SourceCode">Verified Access</span>
       <button onClick={onClickHandler} type="button"><LogInIcon /></button>
--- src/components/waitList/WaitList.js
+++ src/components/waitList/WaitList.js
@@ -31,9 +31,9 @@ const WaitList = () => {
         <div>
         <form className="flex justify-center mt-10 whitespace-nowrap">
         <div className="">
-          <span className="text-18 font-RatDisplay lg:mr-6">Phone Number</span>
-          <input className="lg:w-553 w-167 lg:h-88 h-60 border border-primaryGray rounded-full outline-none pl-6 placeholder-primaryGray text-24 font-400 font-RatDisplay" type="email" placeholder="i.e  +234 0800 000 0000" />
-          <button className="lg:h-72 h-40 lg:w-172 w-50 bg-primaryBlue text-white lg:rounded-full rounded-2xl ml-2" type="submit">Join Waitlist</button>
+          <span className="text-18 font-RatDisplay hidden lg:inline-block mr-0 lg:mr-6">Phone Number</span>
+          <input className="lg:w-553 w-240 lg:h-88 h-60 border border-primaryGray rounded-full outline-none pl-6 placeholder-primaryGray text-24 font-400 font-RatDisplay" type="email" placeholder="email Address" />
+          <button className="lg:h-72 h-50 lg:w-172 w-110 bg-primaryBlue text-white rounded-full ml-2" type="submit">Join Waitlist</button>
         </div>
         </form>
         </div>
