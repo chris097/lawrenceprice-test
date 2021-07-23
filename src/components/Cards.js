10acde95ed8ef1de13ece88a6e13a48dc93ab8ea
chris097 <chrisfidel.international@gmail.com>
2021-07-23 12:36:07 +0100

    fix: adjust card, reduced font size

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,6 +6,7 @@ import Loading from '../components/loading/Loading';
 const Cards = ({onClickHandler}) => {
 
   const {data, isLoading} = useData('photos');
+  console.log(data)
 
   return(
     <div className="bg-secondaryBlue w-full h-auto pt-10 pb-16 lg:px-10 px-4 my-10 mx-auto">
@@ -13,12 +14,12 @@ const Cards = ({onClickHandler}) => {
         {isLoading ? <div className="flex justify-center mx-auto"><Loading /> Loading...</div>
         :
           data?.map((card, index) => (
-            <div key={index} onClick={() => onClickHandler(card)} className="lg:w-315 lg:h-317 w-full h-auto bg-white rounded-20 shadow-primaryShadow lg:mr-8 pb-2 mt-8">
-           <img src={card.thumbnailUrl} className="w-full h-192 rounded-tr-20 rounded-tl-20" alt="" />
-          <div className="mx-6 mt-3.5 cursor-pointer">
-            <div className="font-RatDisplay lg:text-36 text-24 font-500 leading-tight">Fruit Basket</div>
+            <div key={index} onClick={() => onClickHandler(card)} className="lg:w-290 lg:h-317 w-full h-auto bg-white rounded-20 shadow-primaryShadow lg:mr-10 pb-2 mt-8">
+           <img src={card.url} className="w-full h-192 rounded-tr-20 rounded-tl-20" alt="" />
+          <div className="mx-6 mt-5 cursor-pointer">
+            <div className="font-RatDisplay text-24 font-500 leading-tight">Fruit Basket</div>
             <div className="flex justify-between">
-              <div className="text-24 font-RatDisplay font-500 text-primaryColor">#2,500</div>
+              <div className="text-18 font-RatDisplay font-500 text-primaryColor">#2,500</div>
               <div className="mt-5 text-13 flex"><span className="mr-1"><FruitIcon /></span> fruit.ng</div>
             </div>
           </div>
--- src/modal/Card.js
+++ src/modal/Card.js
@@ -12,19 +12,19 @@ const Card = ({onClickHandler, id }) => {
     <>
       <div className="bg-gray-700 opacity-30 fixed top-0 left-0 z-50 w-full h-full"></div>
       <div className="mx-auto flex justify-center">
-        <div className="bg-white fixed rounded-20 shadow-primaryShadow lg:w-525 lg:h-757 w-9 h-auto top-5 mx-auto z-50 pb-4">
+        <div className="bg-white fixed rounded-20 shadow-primaryShadow lg:w-525 w-9 h-auto top-10 mx-auto z-50 pb-4">
           { isLoading ? <div className="flex justify-center mx-auto mt-52"><Loading />Uploading image...</div> : 
           <>
           <button onClick={onClickHandler}  className="flex justify-end absolute right-4 top-3" type="button"><Cancel className="flex justify-end items-end" /></button>
           <div><img src={data.url} className="w-full h-308 rounded-tl-20 rounded-tr-20" alt="card_image" /></div>
           <div className="px-10 mt-5">
-            <div className="font-RatDisplay lg:text-36 text-24 font-500 leading-tight">Fruit Basket</div>
-            <div className="text-24 font-RatDisplay font-500 text-primaryColor">#2,500</div>
-            <div className="lg:w-418 lg:text-24 text-18 font-RatDisplay text-lightGray leading-8 mt-2">High Quality Palm & Almon Nuts, grown in the beautiful Planes of Jos Plateau.</div>
+            <div className="font-RatDisplay text-24 font-500 leading-tight">Fruit Basket</div>
+            <div className="text-18 font-RatDisplay font-500 text-primaryColor">#2,500</div>
+            <div className="lg:w-418 text-18 font-RatDisplay text-lightGray leading-8 mt-2">High Quality Palm & Almon Nuts, grown in the beautiful Planes of Jos Plateau.</div>
             <div className="text-13 flex mt-3"><span className="mr-1"><FruitIcon /></span> fruit.ng</div>
             <div className="flex justify-end mt-3 lg:mt-0">
-              <div className="font-RatDisplay font-500 lg:text-24 text-18 whitespace-nowrap items-center flex mr-4 text-lightGray">Add to Cart <span className="ml-2"><CartIcon /></span></div>
-              <button className="w-172 lg:h-60 rounded-full bg-primaryBlue text-white lg:text-24 text-18">Buy Now</button>
+              <div className="font-RatDisplay font-500 text-18 whitespace-nowrap items-center flex mr-4 text-lightGray">Add to Cart <span className="ml-2"><CartIcon /></span></div>
+              <button className="w-172 p-2 rounded-full bg-primaryBlue text-white text-18">Buy Now</button>
             </div>
           </div>
           </>
